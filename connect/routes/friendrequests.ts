2f3f71d0c6ea6fcab992c379133e5fa07f6d9e98
xVany <rayy12204@gmail.com>
2023-05-30 16:01:42 -0400

    accept friend requests

--- connect/routes/friendrequests.ts
+++ connect/routes/friendrequests.ts
@@ -42,8 +42,49 @@ router.post('/api/addFriend', async (req, res) =>{
         console.error('Error creating friend request:', error);
 
         return res.status(500).json({ message: 'Server error' });
-      }
+    }
 
+})
+
+router.put('/api/acceptRequest/:requestID', async (req, res)=>{
+    try{
+        const {requestID} = req.params;
+        const friendReq = await request.findById(requestID);
+
+        if (!friendReq){
+            return res.status(404).json({ message: 'Friend request not found' });
+        }
+
+        const sender = await User.findById(friendReq.senderID);
+        const receiver = await User.findById(friendReq.receiverID);
+
+        if (!sender || !receiver) {
+            return res.status(404).json({ message: 'Sender or receiver not found' });
+        }
+
+        sender.friends.push(receiver._id);
+        receiver.friends.push(sender._id);
+
+        await sender.save();
+        await receiver.save();
+
+        await request.findByIdAndDelete(requestID);
+
+        await User.findByIdAndUpdate(sender._id, {
+            $pull: { incomingrequests: requestID }
+        });
+
+        await User.findByIdAndUpdate(receiver._id, {
+            $pull: { outgoingrequests: requestID }
+        });
+
+        return res.status(200).json({ message: 'Friend request accepted' });
+        
+    } catch (error) {
+        console.error('Error creating friend request:', error);
+
+        return res.status(500).json({ message: 'Server error' });
+    }
 })
 
 export const friendRequests = router;
