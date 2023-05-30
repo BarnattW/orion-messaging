94440318d335b7f0e9754d771bf03c880431813e
xVany <rayy12204@gmail.com>
2023-05-30 13:27:18 -0400

    fixed the addFriend post request and added the data to the json on a successful request

--- connect/app.ts
+++ connect/app.ts
@@ -7,28 +7,32 @@ require('dotenv').config();
 const app = express();
 const PORT = 3000;
 const URI:string = process.env.DATABASE_URI!
-mongoose.connect(URI).catch((error) => console.error('Connection error:', error));
+mongoose.connect('mongodb://localhost:27017/database').catch((error) => console.error('Connection error:', error));
 
 app.use(express.json());
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
 
 //routes
 
 app.post('/api/addFriend', async (req, res) =>{
     try {
-        const { senderId, receiverId } = req.body;
+        const {senderID, receiverID} = req.body;
     
-        const sender = await User.findById(senderId);
-        const receiver = await User.findById(receiverId);
+        const sender = await User.findById(senderID);
+        const receiver = await User.findById(receiverID);
 
-        if (!sender || !receiver) {
-          return res.status(404).json({ message: 'Sender or receiver not found' });
+        if (!sender|| !receiver) {
+            return res.status(404).json({ message: 'Sender or receiver not found' });
         }
     
         const newRequest = new request({
-          sender: senderId,
-          receiver: receiverId,
-          type: 'friend',
-          status: 'pending'
+            senderID: sender,
+            receiverID: receiver,
+            requestType: 'friend',
+            status: 'pending'
         });
     
         await newRequest.save();
@@ -39,7 +43,12 @@ app.post('/api/addFriend', async (req, res) =>{
         receiver.incomingrequests.push(newRequest._id);
         await receiver.save();
     
-        return res.status(201).json({ message: 'Friend request created' });
+        return res.status(201).json(
+            { 
+                message: 'Friend request created' ,
+                data: newRequest
+            }
+            );
         
     } catch (error) {
         console.error('Error creating friend request:', error);
@@ -48,3 +57,14 @@ app.post('/api/addFriend', async (req, res) =>{
       }
 
 })
+
+
+app.get('/api/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      return res.json(users);
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      return res.status(500).json({ message: 'Server error' });
+    }
+  });
