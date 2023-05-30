dcee5958caf2a360b47f6d1708e2bbafd02e7075
xVany <rayy12204@gmail.com>
2023-05-30 19:51:35 -0400

    user now storess username

--- connect/models/user.ts
+++ connect/models/user.ts
@@ -1,12 +1,17 @@
 import mongoose, {Schema, model} from 'mongoose';
 
 interface IUser{
+    username: string
     friends: Array<mongoose.Types.ObjectId>
     outgoingrequests: Array<mongoose.Types.ObjectId>
     incomingrequests: Array<mongoose.Types.ObjectId>
 }
 
 const UserSchema = new Schema<IUser>({
+    username:{
+        type: String,
+        required: true
+    },
     friends: [{ 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User' 
