a4d2da259063d229f816789e3fe6448863f391d6
xVany <rayy12204@gmail.com>
2023-05-30 21:01:37 -0400

    moved routes and implemented sockets for friend requests

--- connect/app.ts
+++ connect/app.ts
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import {User} from './models/user'
 import { request } from "./models/request";
 import express, {Request, Response } from 'express';
+import http from 'http';
+import {Server, Socket} from 'socket.io';
+import {sendFriendRequest} from './routes/sockets/friendrequests';
 
 const app = express();
 const PORT = 3000;
@@ -9,25 +12,21 @@ const URI:string = process.env.DATABASE_URI!
 mongoose.connect('mongodb://localhost:27017/database').catch((error) => console.error('Connection error:', error));
 
 app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 
 //routes
-import { friendRequests } from "./routes/friendrequests";
+import { friendRequests } from "./routes/api/friendrequests";
 app.use(friendRequests)
-import { friends } from "./routes/friends";
+import { friends } from "./routes/api/friends";
 app.use(friends)
 
+const server = http.createServer(app);
+const io = new Server(server);
 
-
-app.get('/api/users', async (req, res) => {
-    try {
-      const users = await User.find();
-      return res.json(users);
-    } catch (error) {
-      console.error('Error retrieving users:', error);
-      return res.status(500).json({ message: 'Server error' });
-    }
-});
+io.on('connection', (socket: Socket) =>{
+    sendFriendRequest(socket);
+})
--- connect/routes/api/friends.ts
+++ connect/routes/api/friends.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import {User} from '../models/user'
-import { request } from "../models/request";
+import {User} from '../../models/user'
+import { request } from "../../models/request";
 import express, {Request, Response} from 'express';
 
 const router = express.Router();
