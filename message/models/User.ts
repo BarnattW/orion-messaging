9c964f53405727e3fd85920c1a1fb817a4e81cec
Geoffrey Lin <glin91044@gmail.com>
2023-06-06 13:50:40 -0400

    added models

---
+++ message/models/User.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Types } from "mongoose";
+
+interface IUser {
+  userId: string;
+  username: String;
+  conversations: Array<Types.ObjectId>
+}
+
+const UserSchema = new Schema<IUser>({
+  userId: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  conversations: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Conversation'
+  }]
+});
+
+export const User = model<IUser>("User", UserSchema);
