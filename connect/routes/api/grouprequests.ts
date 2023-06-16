3408d818c4414f44ace93cb607527b93c9764a50
xVany <rayy12204@gmail.com>
2023-06-16 19:13:53 -0400

    group request

--- connect/routes/api/grouprequests.ts
+++ connect/routes/api/grouprequests.ts
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
-import {User} from '../../models/user'
+import { User } from "../../models/user";
 import { request } from "../../models/request";
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import { publishMessage } from "./idkwhattonamethis/kafkaproducer";
+import { insertionSort } from "./idkwhattonamethis/sort";
+
+import { sendRequest } from "./friendrequests";
 
 const router = express.Router();
+
+router.post("/api/connect/sendGroupRequest", (req: Request, res: Response) =>
+	sendRequest(req, res, "group")
+);
