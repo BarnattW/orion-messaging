797dc9d753506b4c0693e1640a54843954be6750
Barndude <bwu22@stevens.edu>
2023-07-21 16:51:04 -0400

    small changes

--- connect/src/redis/lrucache.ts
+++ connect/src/redis/lrucache.ts
@@ -2,36 +2,39 @@ import Redis from 'ioredis';
 import { User } from '../models/user';
 
 const redisClient = new Redis({
-    host: 'localhost',
-    port: 6379
+	host: process.env.REDIS_HOST,
+	port: 6379,
 });
 
-redisClient.config('SET', 'maxmemory', '256mb');
-redisClient.config('REWRITE');
-redisClient.quit();
-
+redisClient.ping((err, result) => {
+	if (err) {
+		console.error("Error connecting to Redis:", err);
+	} else {
+		console.log("Connected to Redis");
+	}
+});
 
 const maxSize = 100;
-const cacheKey = 'userCache';
+const cacheKey = "userCache";
 
-export async function getUsername(userId: string){
-    // Check the cache for the username
-    const cachedUsername = await redisClient.hget('usernames', userId);
-  
-    if (cachedUsername) {
-      // If the username is found in the cache, return it
-      await updateRank(userId);
-      return cachedUsername;
-    }
-  
-    // Cache miss - fetch the username from the data source
-    const user = await User.findOne({userId: userId});
-  
-    if (user) {
-      // Set the fetched username in the cache
-      await setUsername(userId, user.username);
-      return user.username;
-    }
+export async function getUsername(userId: string) {
+	// Check the cache for the username
+	const cachedUsername = await redisClient.hget("usernames", userId);
+
+	if (cachedUsername) {
+		// If the username is found in the cache, return it
+		await updateRank(userId);
+		return cachedUsername;
+	}
+
+	// Cache miss - fetch the username from the data source
+	const user = await User.findOne({ userId: userId });
+
+	if (user) {
+		// Set the fetched username in the cache
+		await setUsername(userId, user.username);
+		return user.username;
+	}
 }
 
 export async function setUsername(userId: string, username: string){
